c6e1d484d8d2923a68192a3897c5d1bc2485afd3
Ari Suutari <ari@stonepile.fi>
2019-04-13 09:38:38 +0300

    Add door sensor event handling.

--- sb1_uart.h
+++ sb1_uart.h
@@ -40,8 +40,10 @@ enum SB1MessageType {
 };
 
 enum SB1EventType {
+  SB1_EVENT_TYPE_DOOR   = 0x0104,
   SB1_EVENT_TYPE_MOTION = 0x6501,
-  SB1_EVENT_TYPE_RESET  = 0x6604
+  SB1_EVENT_TYPE_DOOR_RESET   = 0x6504,
+  SB1_EVENT_TYPE_MOTION_RESET = 0x6604
 };
 
 enum SB1State {
@@ -390,7 +392,14 @@ class SB1UARTComponent : public Component, public UARTDevice {
                 if (this->sensor_ != nullptr) {
                   this->sensor_->publish_state(this->message_.value[i + 4] > 0);
                 }
-              } else if (event_type == SB1_EVENT_TYPE_RESET) {
+              }
+              else if (event_type == SB1_EVENT_TYPE_DOOR) {
+                ESP_LOGI(TAG, "Door event: %d", this->message_.value[i + 4]);
+                if (this->sensor_ != nullptr) {
+                  this->sensor_->publish_state(this->message_.value[i + 4] == 0);
+                }
+              } else if (event_type == SB1_EVENT_TYPE_MOTION_RESET ||
+                         event_type == SB1_EVENT_TYPE_DOOR_RESET) {
                 ESP_LOGI(TAG, "Reset event: %d", this->message_.value[i + 4]);
               }
             }
